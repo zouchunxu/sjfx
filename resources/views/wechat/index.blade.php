60d9db180aa876a970f67d439791502e410c52f0
zou.cx <471500439@qq.com>
2017-04-29 22:38:27 +0800

    update

--- resources/views/wechat/index.blade.php
+++ resources/views/wechat/index.blade.php
@@ -92,7 +92,7 @@
                 </div>
                 <div class="other">
                     <div class="price">
-                         ¥88.00
+                        ¥88.00
                     </div>
                     <div class="detail">
                         详情 >
@@ -110,7 +110,7 @@
                 </div>
                 <div class="other">
                     <div class="price">
-                         ¥88.00
+                        ¥88.00
                     </div>
                     <div class="detail">
                         详情 >
@@ -128,7 +128,7 @@
                 </div>
                 <div class="other">
                     <div class="price">
-                         ¥88.00
+                        ¥88.00
                     </div>
                     <div class="detail">
                         详情 >
@@ -147,13 +147,48 @@
                 </div>
                 <div class="other">
                     <div class="price">
-                         ¥88.00
+                        ¥88.00
                     </div>
                     <div class="detail">
                         详情 >
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+</div>
+<div style="height: 80px;"></div>
+<div class="bottom-nav">
+    <div class="nav-item">
+        <div class="icon">
+            <span class="fa fa-home"></span>
+        </div>
+        <div class="title">
+            农场主页
+        </div>
+    </div>
+    <div class="nav-item active">
+        <div class="icon">
+            <span class="fa fa-shopping-cart"></span>
+        </div>
+        <div class="title">
+            现代商城
+        </div>
+    </div>
+    <div class="nav-item">
+        <div class="icon">
+            <span class="fa fa-user"></span>
+        </div>
+        <div class="title">
+            我的庄园
+        </div>
+    </div>
+    <div class="nav-item">
+        <div class="icon">
+            <span class="fa fa-tree"></span>
+        </div>
+        <div class="title">
+            庄主家园
         </div>
     </div>
 </div>
@@ -173,6 +208,11 @@
             fluid: true,
             dots: true
         });
+        $('.nav-item').click(function () {
+           $('.bottom-nav').children(0).removeClass('active');
+           $(this).addClass('active');
+        });
+
     });
 
 </script>
