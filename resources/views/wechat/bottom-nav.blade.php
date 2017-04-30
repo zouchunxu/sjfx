21ee8179a8764a2d848e8052a3a40d79f437f41c
zou.cx <471500439@qq.com>
2017-04-30 23:49:58 +0800

    update

--- resources/views/wechat/bottom-nav.blade.php
+++ resources/views/wechat/bottom-nav.blade.php
@@ -1,21 +1,25 @@
 <div style="clear:both;height: 60px;"></div>
 <div class="bottom-nav">
-    <div class="nav-item active">
-        <div class="icon">
-            <span class="fa fa-home"></span>
+    <a href="/index">
+        <div class="nav-item">
+            <div class="icon">
+                <span class="fa fa-home"></span>
+            </div>
+            <div class="title">
+                农场主页
+            </div>
         </div>
-        <div class="title">
-            农场主页
+    </a>
+    <a href="/good">
+        <div class="nav-item">
+            <div class="icon">
+                <span class="fa fa-shopping-cart"></span>
+            </div>
+            <div class="title">
+                现代商城
+            </div>
         </div>
-    </div>
-    <div class="nav-item">
-        <div class="icon">
-            <span class="fa fa-shopping-cart"></span>
-        </div>
-        <div class="title">
-            现代商城
-        </div>
-    </div>
+    </a>
     <div class="nav-item">
         <div class="icon">
             <span class="fa fa-user"></span>
@@ -24,20 +28,33 @@
             我的庄园
         </div>
     </div>
-    <div class="nav-item">
-        <div class="icon">
-            <span class="fa fa-tree"></span>
+    <a href="/user">
+        <div class="nav-item">
+            <div class="icon">
+                <span class="fa fa-tree"></span>
+            </div>
+            <div class="title">
+                庄主家园
+            </div>
         </div>
-        <div class="title">
-            庄主家园
-        </div>
-    </div>
+    </a>
+
+
 </div>
 <script>
     $(document).ready(function () {
-        $('.nav-item').click(function () {
-            $('.bottom-nav').children(0).removeClass('active');
-            $(this).addClass('active');
+//        $('.nav-item').click(function () {
+//            $('.bottom-nav').children(0).removeClass('active');
+//            $(this).addClass('active');
+//        });
+
+        var wechatCurrentUrl = window.location.pathname;
+
+        $('.bottom-nav a').each(function () {
+            if (typeof $(this).attr('href') == 'string' && wechatCurrentUrl.match($(this).attr('href'))) {
+                $(this).find('.nav-item').addClass('active');
+            }
         });
+
     });
 </script>
--- resources/views/wechat/good.blade.php
+++ resources/views/wechat/good.blade.php
@@ -16,42 +16,24 @@
         <div class="col-4"
              onClick="selectTab(0)">
             <span>认养商城</span><br/>
-            <i class="iconfont">&#xe6a6;</i>
+            <i class="iconfont"></i>
         </div>
         <div class="col-4"
              onClick="selectTab(1)">
             <span>积分商城</span><br/>
-            <i class="iconfont">&#xe6a6;</i>
+            <i class="iconfont"></i>
         </div>
         <div class="col-4"
              onClick="selectTab(2)">
             <span>养护商城</span><br/>
-            <i class="iconfont">&#xe6a6;</i>
+            <i class="iconfont"></i>
         </div>
         <div class="tab-content clear">
             <div class="content-header">
                 类型1
-                <i class="iconfont pull-right mr-5">&#xe6a7;</i>
+                <i class="iconfont pull-right mr-5"></i>
             </div>
             <div class="goods">
-                <div class="list-item">
-                    <div class="logo">
-                        <img src="/assets/img/unslider/shop.jpg" alt="">
-                    </div>
-                    <div class="title">
-                        <div class="name">
-                            测试一下
-                        </div>
-                        <div class="other">
-                            <div class="price">
-                                ¥88.00
-                            </div>
-                            <div class="detail">
-                                详情 >
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <div class="list-item">
                     <div class="logo">
                         <img src="/assets/img/unslider/shop.jpg" alt="">
@@ -93,7 +75,7 @@
         <div class="tab-content clear">
             <div class="content-header">
                 类型2
-                <i class="iconfont pull-right mr-5">&#xe6a7;</i>
+                <i class="iconfont pull-right mr-5"></i>
             </div>
             <div class="goods">
                 <div class="col-2">
@@ -110,7 +92,7 @@
         <div class="tab-content clear">
             <div class="content-header">
                 类型3
-                <i class="iconfont pull-right mr-5">&#xe6a7;</i>
+                <i class="iconfont pull-right mr-5"></i>
             </div>
             <div class="goods">
                 <div class="col-2">
