a135c9f37227daf08e2d645d7a023a5865742093
zou.cx <471500439@qq.com>
2017-06-07 23:23:30 +0800

    update

--- app/Console/Commands/TradeCreate.php
+++ app/Console/Commands/TradeCreate.php
@@ -31,12 +31,29 @@ class TradeCreate extends Command
      */
     public function handle()
     {
-        $redis = (new RedisHelp())->getRedis();
+//        $redis = (new RedisHelp())->getRedis();
+
 
         while (true) {
-            $data = $redis->brPop('trade', 30);
-            if (array_get($data,1)) {
-                $val = json_decode($data[1], true);
+//            $data = $redis->brPop('trade', 30);
+//            if (array_get($data,1)) {
+//                $val = json_decode($data[1], true);
+            $val = array (
+                'trade_no' => 'C1009621741',
+                'extra_return_param' => '5',
+                'sign_type' => 'RSA-S',
+                'notify_type' => 'offline_notify',
+                'merchant_code' => '2020000568',
+                'order_no' => '20170607231520623',
+                'trade_status' => 'SUCCESS',
+                'sign' => 'k/RTkKH0bkBLUqYmGvsxl5FbGp4GiQc1usiLAi2A7Wpjsvle+y50AMvclqKm4iTV7XOTQxY3wr4tkBUWFtMw7/gEcxeRga3F8rCXdK+elZQLOV1RR4Tws6W/Zj+UkWyak2qdjZ/3vPabT8HscxzLwY475OVNo72LFobeZHN+Q2g=',
+                'order_amount' => '0.01',
+                'interface_version' => 'V3.0',
+                'bank_seq_no' => '8021800843211170607095029330',
+                'order_time' => '2017-06-07 23:15:20',
+                'notify_id' => '300e911a37cb4244bdb6fac5c27423cb',
+                'trade_time' => '2017-06-07 23:15:05',
+            );
                 var_export($val);
                 $trade = new Trade();
                 $trade->uid = $val['extra_return_param'];
@@ -48,6 +65,9 @@ class TradeCreate extends Command
                 var_dump( number_format($val['order_amount']*0.03,2));
                 //一级
                 $oneUser = $user->tallLevelUser;
+
+
+                die;
                 if (empty($oneUser)) {
                     continue;
                 }
@@ -70,7 +90,7 @@ class TradeCreate extends Command
                 var_dump($threeUser->save());
 
 
-            }
+//            }
 
         }
     }
