db6baf9311aeca6e23a54fdeddbeb9bd51f3b72a
zou.cx <471500439@qq.com>
2017-06-07 21:37:26 +0800

    update

---
+++ database/migrations/2017_06_07_212548_create_trade.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrade extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trade', function (Blueprint $table) {
+            $table->bigInteger('id');
+            $table->string('open_id');
+            $table->double('price');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
