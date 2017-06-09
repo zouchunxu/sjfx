cb2364e709fc0102274f95af619e6d6c5dbbf896
zou.cx <471500439@qq.com>
2017-06-09 21:41:24 +0800

    update

--- app/Http/Controllers/Admin/WareController.php
+++ app/Http/Controllers/Admin/WareController.php
@@ -60,7 +60,7 @@ class WareController extends Controller
         if ($request->isMethod('get')) {
             return view('admin/ware-list')->withUrl($request->url());
         } else {
-            $data = Ware::paginate(15);
+            $data = Ware::query()->paginate(1500);
             return response()->json(['data' => $data->toJson(), 'page' => $data->render()]);
         }
     }
